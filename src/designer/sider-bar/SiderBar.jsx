26f8ee78cefca1ea4893bb0840a7d275717253e0
大漠穷秋 <damoqiongqiu@126.com>
2022-02-26 17:43:00 +0800

    refactor-siderbar-style

--- src/designer/sider-bar/SiderBar.jsx
+++ src/designer/sider-bar/SiderBar.jsx
@@ -10,8 +10,10 @@ const SiderBarWrapper = styled.div`
   height: 100%;
   overflow-y: auto;
   overflow-x: hidden;
+  background-color: #cfdaf0;
 
   /* override antd default tab styles */
+  /* FIXME:refactor the css using LESS */
   .ant-tabs > .ant-tabs-nav .ant-tabs-nav-list,
   .ant-tabs > div > .ant-tabs-nav .ant-tabs-nav-list {
     position: relative;
@@ -23,8 +25,23 @@ const SiderBarWrapper = styled.div`
   .ant-tabs-card > div > .ant-tabs-nav .ant-tabs-tab {
     margin: 0;
     padding: 8px 16px;
-    border-right: 1px solid #f0f0f0;
+    background: #696185;
+    border: none;
     transition: all 0.3s cubic-bezier(0.645, 0.045, 0.355, 1);
+    color: #f7f7f7;
+  }
+  .ant-tabs-card.ant-tabs-top > .ant-tabs-nav .ant-tabs-tab,
+  .ant-tabs-card.ant-tabs-top > div > .ant-tabs-nav .ant-tabs-tab {
+    border-radius: 0;
+  }
+  .ant-tabs-card > .ant-tabs-nav .ant-tabs-tab-active,
+  .ant-tabs-card > div > .ant-tabs-nav .ant-tabs-tab-active {
+    color: #fff;
+    background: #847ca3;
+  }
+  .ant-tabs-tab.ant-tabs-tab-active .ant-tabs-tab-btn {
+    color: #fff;
+    text-shadow: 0 0 0.25px currentcolor;
   }
   .ant-tabs-card.ant-tabs-top > .ant-tabs-nav .ant-tabs-tab + .ant-tabs-tab,
   .ant-tabs-card.ant-tabs-bottom > .ant-tabs-nav .ant-tabs-tab + .ant-tabs-tab,
